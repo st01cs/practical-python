08996c4f611098c912fbcd0081986cef1fce5a12
st01cs <42166106+st01cs@users.noreply.github.com>
2020-07-16 10:58:20 +0800

    Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,13 +3,17 @@
 # Exercise 1.27
 import os
 script_dir = os.path.dirname(__file__)
-f = open(script_dir +'/Data/portfolio.csv', 'rt')
-heads = next(f)
 
-total_cost = 0.0
+def portfolio_cost(filename):
+    f = open(script_dir +'/'+filename, 'rt')
+    heads = next(f)
 
-for line in f:
-    name, shares, price = line.rstrip().split(',')
-    total_cost = total_cost + int(shares) * float(price)
+    total_cost = 0.0
 
-print(total_cost)
+    for line in f:
+        name, shares, price = line.rstrip().split(',')
+        total_cost = total_cost + int(shares) * float(price)
+    return total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
