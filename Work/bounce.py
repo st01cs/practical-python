e581871d5fbc5dc3cd2ad05d1a8651c822d33f85
st01cs <42166106+st01cs@users.noreply.github.com>
2020-07-16 10:36:55 +0800

    Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+for i in range(1,11):
+    next_height = height * 0.6
+    print("%d\t%.4f" % (i, round(next_height, ndigits=4)))
+    height = next_height
